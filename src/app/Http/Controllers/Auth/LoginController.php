d98b4b5fcb62cd07b3c9fea380743addbede4493
anandadwipra <anandabiru04@gmail.com>
2021-10-06 18:57:35 +0700

    :hammer: Fix Login custom Field

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,10 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-
+    public function username()
+    {
+        return 'username';
+    }
     /**
      * Create a new controller instance.
      *
