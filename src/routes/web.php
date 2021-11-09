42bcbd92bc57124e70ad5678b5b41afb4df5f1be
Ananda Dwi <anandabiru04@gmail.com>
2021-11-09 12:00:46 +0700

    test github action

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('home');
+    #return redirect()->route('home');
+    return "Index Page";
 });
 
 Auth::routes();
