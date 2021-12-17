b281091eb64e86fe9f570de039e17d6c6026401c
Faisal L <salluthdev@gmail.com>
2021-12-17 11:20:27 +0700

    Penghapusan Padding Money Report - Wallet V2

--- src/resources/views/panel/wallet/index.blade.php
+++ src/resources/views/panel/wallet/index.blade.php
@@ -181,7 +181,7 @@
             </div>
             <!-- /.card -->
         </div>
-        <div class="col-md-12">
+        <div class="col-md-12 no-padding">
             <div class="card card-primary collapsed-card">
                 <div class="card-header">
                     <h3 class="card-title">Money Report</h3>
@@ -201,7 +201,7 @@
             <!-- /.card -->
         </div>
 
-        <div class="col-md-12">
+        <div class="col-md-12 no-padding">
             <div class="card card-primary collapsed-card">
                 <div class="card-header">
                     <h3 class="card-title">Money Report V2</h3>
