d3ad02533f7b45dab11930ff15c3a41ef9c3001d
Ananda Dwi <anandabiru04@gmail.com>
2022-01-06 09:27:14 +0700

    change path '/'
    Signed-off-by: Ananda Dwi <anandabiru04@gmail.com>

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->route('home');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
